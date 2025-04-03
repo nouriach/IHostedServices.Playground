3587b30a823ff0d6cfd1fa25b4a329e8184c4140
nouriach <nouriach17@gmail.com>
2025-04-03 11:18:48 +0100

    Register Client & add `JsonPropertyName` attribute

--- src/IHostedServices.Playground/DTOs/BreweryDto.cs
+++ src/IHostedServices.Playground/DTOs/BreweryDto.cs
@@ -1,6 +1,9 @@
+using System.Text.Json.Serialization;
+
 namespace IHostedServices.Playground.DTOs;
 
 public class BreweryDto
 {
+    [JsonPropertyName("website_url")]
     public string WebsiteUrl { get; set; } = null!;
 }
--- src/IHostedServices.Playground/Program.cs
+++ src/IHostedServices.Playground/Program.cs
@@ -1,4 +1,5 @@
-﻿using IHostedServices.Playground.Configuration;
+﻿using IHostedServices.Playground.Clients;
+using IHostedServices.Playground.Configuration;
 using IHostedServices.Playground.HostedServices;
 
 var builder = WebApplication.CreateBuilder();
@@ -10,8 +11,35 @@ builder.Services.Configure<BreweryApiConfiguration>(
     builder.Configuration.GetSection(BreweryApiConfiguration.SectionName)
 );
 
+builder.Services.AddHttpClient<IBreweryApiClient, BreweryApiClient>();
+
+builder.Services.AddCors(options =>
+{
+    var domains = new List<string>();
+    var index = 0;
+    
+    while (true)
+    {
+        var key = $"AllowedOrigins__{index}";
+        var value = Environment.GetEnvironmentVariable(key);
+        if (value is null) break;
+        
+        domains.Add(value);
+        index++;
+    }
+
+    options.AddPolicy("AllowedClientDomains", policy =>
+    {
+        policy.WithOrigins(domains.ToArray())
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
+
 var app = builder.Build();
 
 // build your app
+
+app.UseCors("AllowedClientDomains");
 
 app.Run();
